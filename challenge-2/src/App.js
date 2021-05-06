7c15772b13b94a7236bac3e324c212fb48f94e47
bradmic16 <bradmic16@gmail.com>
2021-05-05 20:44:10 -0500

    end-of-challenge-2

---
+++ challenge-2/src/App.js
@@ -0,0 +1,12 @@
+import MessageList from './components/MessageList';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <MessageList />
+    </div>
+  );
+}
+
+export default App;
